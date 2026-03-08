3b28c7b4f6b2e620e7f5e5133fe0d5118d4f8288
Matt Gus <matt@spacerake.net>
2026-03-08 18:33:36 -0400

    supabase fix

--- supabase.js
+++ supabase.js
@@ -1,7 +1,5 @@
 const SUPABASE_URL = "https://ecfrcyeltvmcaymmedcc.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ghriEXc9lI6DUknYbn2MeQ_82L935w9";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+// Create Supabase client and expose it globally
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
